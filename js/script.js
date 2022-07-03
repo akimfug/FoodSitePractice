ba788624dab9e7daa93270754a0796d8282250d8
AkimFug <akim.fuzhenko@gmail.com>
2022-07-03 21:32:46 +0300

    createdTabsSlider

--- js/script.js
+++ js/script.js
@@ -2,8 +2,39 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    const tabs = document.querySelectorAll('.tabheader__items'), // СПИСОК ПИТАНИЯ? 0: Фитнес, 1: Премиум, 2: Постное...
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParrent = document.querySelectorAll('.tabheader__item');
+    let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        }); // Отключение прямоугольников
+    
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        }); // Отключение текстов
+    }
+    
+    function showTabContent (i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    
+    hideTabContent();
+    showTabContent(1);
+
+    tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+	});
 });
