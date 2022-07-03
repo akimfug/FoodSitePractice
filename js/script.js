c103e3f543eaaa01d0ebe18f5c6d504871f914b5
AkimFug <akim.fuzhenko@gmail.com>
2022-07-03 19:10:16 +0300

    WithoutJavaScript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__items'), // СПИСОК ПИТАНИЯ? 0: Фитнес, 1: Премиум, 2: Постное...
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParrent = document.querySelectorAll('.tabheader__item');
+
+});
